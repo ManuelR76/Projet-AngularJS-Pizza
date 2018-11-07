df5e3d8a1b679e2a0496a11af93565609f0aa08c
ManuelR76 <romeromanuel76@gmail.com>
2018-11-07 09:03:57 +0100

    last version

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// $( document ).ready(function($) {
+//     $(window).scroll(function(){
+//       if ($(this).scrollTop() > 570) {
+//         $('.navbar').fadeIn(500);
+//       } else {
+//         $('.navbar').fadeOut(500);
+//       }
+//     })
+//     ;
+//   });
+
+  // $(window).scroll(function() {
+  // // 100 = The point you would like to fade the nav in.
+  //
+  // 	if ($(window).scrollTop() > 200 ){
+  //
+  //  		$('.navbar').addClass('show');
+  //
+  //   } else {
+  //
+  //     $('.navbar').removeClass('show');
+  //
+  //  	};
+  // });
